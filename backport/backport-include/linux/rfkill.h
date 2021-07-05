d3480ef5f9024dfe712700832473c90c577d8ca5
Ben Greear <greearb@candelatech.com>
2021-07-05 11:12:14 -0700

    Fix rfkill for earlier than 5.11 kernels
    From Johannes Berg <johannes@sipsolutions.net>
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- backport/backport-include/linux/rfkill.h
+++ backport/backport-include/linux/rfkill.h
@@ -164,4 +164,23 @@ static inline bool backport_rfkill_blocked(struct rfkill *rfkill)
 #define rfkill_blocked backport_rfkill_blocked
 #endif
 
+#if LINUX_VERSION_IS_LESS(5,11,0)
+
+/* This should come from uapi/linux/rfkill.h, but it was much easier
+ * to do it this way.
+ */
+enum rfkill_hard_block_reasons {
+        RFKILL_HARD_BLOCK_SIGNAL        = 1 << 0,
+        RFKILL_HARD_BLOCK_NOT_OWNER     = 1 << 1,
+};
+
+static inline bool rfkill_set_hw_state_reason(struct rfkill *rfkill,
+                                              bool blocked, unsigned
+long reason)
+{
+        return rfkill_set_hw_state(rfkill, blocked);
+}
+
+#endif /* 5.11 */
+
 #endif
